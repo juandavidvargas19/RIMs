c72805789a2160dd6fd9e00747d053f6f340b23a
juandavidvargas19 <114268705+juandavidvargas19@users.noreply.github.com>
2024-04-03 14:52:27 -0400

    Update train_copying.py

--- event_based/train_copying.py
+++ event_based/train_copying.py
@@ -171,14 +171,12 @@ if args.algo == "blocks":
                             layer_dilation=args.layer_dilation, num_modules_read_input=args.read_input).to(device)
 elif args.algo == "lstm":
     #rnn_mod = baseline_lstm_model.RNNModel
-    rnn_mod = baseline_lstm_model_adding.RNNModel    
-    dropout_value = args.dropout[0] if isinstance(args.dropout, list) else args.dropout
-    #model = rnn_mod('LSTM', ntokens, args.emsize, args.nhid, args.nlayers, dropout_value, args.tied).to(device)
     
-    model = rnn_mod(args.model, ntokens, args.emsize, args.nhid,
-                        args.nlayers, args.dropout, args.tied,
-                        use_cudnn_version=args.cudnn,
-                        use_adaptive_softmax=args.adaptivesoftmax, cutoffs=args.cutoffs).to(device)
+    rnn_mod = baseline_lstm_model.RNNModel    
+    dropout_value = args.dropout[0] if isinstance(args.dropout, list) else args.dropout
+    model = rnn_mod('LSTM', ntokens, args.emsize, args.nhid, args.nlayers, dropout_value, args.tied).to(device)
+    
+    #model = rnn_mod(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.tied, use_cudnn_version=args.cudnn, use_adaptive_softmax=args.adaptivesoftmax, cutoffs=args.cutoffs).to(device)
     
 else:
     raise Exception("Algorithm option not found")
